419f75220250c7e9bc9c12d2383460db6bd4fa77
Alexandra Martinez <nahomibj@gmail.com>
2017-02-02 23:00:58 -0600

    Added configuration to use properties using annotations

--- src/main/java/com/martinez/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/martinez/repository/HibernateCustomerRepositoryImpl.java
@@ -5,13 +5,20 @@ import com.martinez.model.Customer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${myString}")
+    private String myString;
+
     @Override
     public List<Customer> findAll() {
+
+        System.out.println(myString);
+
         List<Customer> customers = new ArrayList<>();
 
         Customer customer = new Customer();
